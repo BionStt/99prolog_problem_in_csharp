4a9660ed54177be7da573085562f2127d5af737e
Asif Rahman <asif0531@live.com>
2019-02-01 23:47:56 +0600

    initial commit

---
+++ 99prolog_problem/99prolog_problem/99prolog_problem/1.Prolog Lists/problem_1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _99prolog_problem._1.Prolog_Lists
+{
+    public class problem_1
+    {
+        public static int FindLastElementFromList(List<int> elements)
+        {
+            return elements[elements.Count-1];
+        }
+    }
+}
---
+++ 99prolog_problem/99prolog_problem/99prolog_problem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _99prolog_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
